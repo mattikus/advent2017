d24e329664f7411958c213e5e64bd291089c8ef5
Matt Kemp <matt@mattikus.com>
2017-12-04 20:41:56 -0600

    Add day 4

---
+++ day_4/2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+def check_passphrase(phrase):
+    words = ["".join(sorted(word)) for word in phrase.split()]
+    return len(set(words)) == len(words)
+
+if __name__ == "__main__":
+    print(len([phrase for phrase in sys.stdin.readlines() if check_passphrase(phrase)]))
