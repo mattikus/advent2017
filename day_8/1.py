da00bea272184ef4d259e3799811ed49fa45ebdc
Matt Kemp <matt@mattikus.com>
2017-12-08 00:58:10 -0600

    Add both day8 solutions

---
+++ day_8/1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+from collections import defaultdict
+
+OP_TABLE = {
+    '>': '__gt__',
+    '<': '__lt__',
+    '>=': '__ge__',
+    '<=': '__le__',
+    '==': '__eq__',
+    '!=': '__ne__',
+}
+
+def process(instructions):
+    registers = defaultdict(int)
+
+    def passes_conditional(reg, op, value):
+        return getattr(registers[reg], OP_TABLE[op])(int(value))
+
+
+    for line in instructions:
+        ops = line.split()
+        register = ops[0]
+        value = int(ops[2])
+        if passes_conditional(*ops[4:]):
+            if ops[1] == 'inc':
+                registers[register] += value
+            else:
+                registers[register] -= value
+
+    return max(registers.values())
+
+
+
+if __name__ == "__main__":
+    instructions = sys.stdin.readlines()
+    print(process(instructions))
+
